180ae25d4967f28a4a65c26b63034f392c0984d4
Artem Pstygo <apstygo.dev@gmail.com>
2023-12-29 22:10:38 +0400

    implement highlighting and sizing

--- Peekaboo/AppDelegate.swift
+++ Peekaboo/AppDelegate.swift
@@ -6,18 +6,29 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 300, height: 300),
+            contentRect: .zero,
             styleMask: [.borderless, .hudWindow, .nonactivatingPanel],
             backing: .buffered,
             defer: false
         )
 
+        #if DEBUG
         window.isMovableByWindowBackground = true
+        #endif
+
         window.hasShadow = false
         window.collectionBehavior = [.auxiliary, .stationary, .transient, .fullScreenNone, .ignoresCycle]
         window.level = .statusBar
-        window.backgroundColor = .magenta
-        window.center()
+        window.contentView = ContentView()
+        window.backgroundColor = .clear
+
+        // Brow size is 184x32
+
+        let screenSize = NSScreen.main?.frame.size ?? .zero
+        let size = CGSize(width: 192, height: 36)
+        let origin = NSPoint(x: (screenSize.width - size.width) / 2, y: screenSize.height - size.height)
+        window.setFrame(NSRect(origin: origin, size: size), display: true)
+
         window.orderFront(nil)
     }
 
@@ -29,4 +40,52 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return true
     }
 
+}
+
+private final class ContentView: NSView {
+    private var trackingArea: NSTrackingArea?
+
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+
+        wantsLayer = true
+        layer?.cornerRadius = 14
+        layer?.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func updateTrackingAreas() {
+        super.updateTrackingAreas()
+
+        if let trackingArea {
+            removeTrackingArea(trackingArea)
+        }
+
+        let trackingArea = makeTrackingArea()
+        addTrackingArea(trackingArea)
+        self.trackingArea = trackingArea
+    }
+
+    override func mouseEntered(with event: NSEvent) {
+        super.mouseEntered(with: event)
+
+        layer?.backgroundColor = NSColor.controlAccentColor.cgColor
+    }
+
+    override func mouseExited(with event: NSEvent) {
+        super.mouseExited(with: event)
+
+        layer?.backgroundColor = nil
+    }
+
+    private func makeTrackingArea() -> NSTrackingArea {
+        NSTrackingArea(
+            rect: bounds,
+            options: [.activeAlways, .mouseEnteredAndExited],
+            owner: self
+        )
+    }
 }
